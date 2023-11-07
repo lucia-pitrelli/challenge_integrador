e6cf6c520ba237324621cb3105004ce3c81dc6f9
Elias Solis <elias3847040@gmail.com>
2023-11-07 02:49:49 -0300

    creacion de rutas y controladores

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+const mainRoutes = require("./src/routes/mainRoutes");
+const shopRoutes = require("./src/routes/shopRoutes");
+const adminRoutes = require("./src/routes/adminRoutes");
+const authRoutes = require("./src/routes/authRoutes");
+
+
+
 
 app.use(express.static('public'));
+app.use('/',mainRoutes);
+app.use('/',shopRoutes);
+app.use('/',adminRoutes);
+app.use('/',authRoutes);
+
+
 
 app.listen(4000, ()=> console.log("servidor corriendo en http://localhost:4000"));
